db1d4e974a81009ba17c6859e6d59f8ab8494a32
John Lloyd <5355693@gmail.com>
2016-12-01 13:12:52 -0500

    Updated analysis code

--- VMC_presentation_script.R
+++ VMC_presentation_script.R
@@ -2,6 +2,9 @@ aud1 <- read.csv(file = "/Users/johnlloyd/GitHub/Mansfield.Phenology/Data/Vermon
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(scales)
+library(splines)
+library(MASS)
 #Data manipulation and summary
 ##First need to summarize instances of BITH/Other detections, 
 ##while adding zeros for missing levels of each factor. That is,
@@ -19,7 +22,11 @@ summary1 <- mutate(summary1, Date = as.Date(Date, format = "%m/%d/%Y"))
 ##Make figures showing number of instances of BITH vocalizations
 summary1 %>%
   filter(Class == "BITH") %>%
-  ggplot(., aes(x = Date, y = instances)) + geom_line() + ylab("No. of Bicknell's Thrush vocalizations")
+  ggplot(., aes(x = Date, y = instances)) + geom_line() + ylab("No. of Bicknell's Thrush vocalizations") + 
+  theme(axis.text.y = element_text(size = 12),
+        axis.text.x = element_text(size = 12, angle = 305,
+                                   vjust = 0.5),
+        axis.title=element_text(size=18,face="bold"))
 
 summary1 %>%
   filter(Class == "OTHER") %>%
@@ -27,11 +34,78 @@ summary1 %>%
 
 cp.bp <- read.csv(file = "/Users/johnlloyd/GitHub/Mansfield.Phenology/Data/CB_BP.csv", header = T, sep = ",")
 
+##Create a figure of CP development among BITH
 cp.bp %>%
+  mutate(Date = as.Date(Date, format = "%m/%d/%Y"))%>%
   group_by(Species, Date, CP) %>%
+  mutate(Condition = ifelse(CP == 0, "NB",ifelse(CP == 1, "NB","Breeding")))%>%
+  group_by(Species, Date, Condition)%>%
   summarize(N = n()) %>%
   mutate(Percentage = N/sum(N)) %>%
-  complete(Species, Date, CP, fill = list(N = 0,Percentage = 0)) %>%
-  filter(Species == "BITH") %>%
-  ggplot(., aes(x = CP, y = Percentage)) + geom_bar(stat_bin())
+  complete(Species, Date, Condition, fill = list(N = 0,Percentage = 0)) %>%
+  filter(Species == "BITH", Condition == "Breeding") %>%
+  ggplot(., aes(x = Date, y = Percentage)) + geom_point() + stat_smooth(method="glm",
+                                                                        method.args = list(family="binomial"), 
+                                                                        formula = y ~ ns(x, 3), level = 0.75) + 
+  ylab("Percentage of captured male Bicknell's\nThrush in breeding condition") + 
+  scale_x_date(breaks = date_breaks("4 days")) + theme(axis.text.y = element_text(size = 12),
+                                                       axis.text.x = element_text(size = 12, angle = 305,
+                                                                                  vjust = 0.5),
+                                                       axis.title=element_text(size=18,face="bold"))
 
+##Create a figure of BP development among BITH
+cp.bp %>%
+  mutate(Date = as.Date(Date, format = "%m/%d/%Y"))%>%
+  group_by(Species, Date, BP) %>%
+  mutate(Condition = ifelse(BP == 0, "NB","Breeding"))%>%
+  group_by(Species, Date, Condition)%>%
+  summarize(N = n()) %>%
+  mutate(Percentage = N/sum(N)) %>%
+  complete(Species, Date, Condition, fill = list(N = 0,Percentage = 0)) %>%
+  filter(Species == "BITH", Condition == "Breeding") %>%
+  ggplot(., aes(x = Date, y = Percentage)) + geom_point() + stat_smooth(method="glm",
+                                                                        method.args = list(family="binomial"), 
+                                                                        formula = y ~ ns(x, 3), level = 0.75) + 
+  ylab("Percentage of captured female Bicknell's\nThrush in breeding condition") + 
+  scale_x_date(breaks = date_breaks("4 days")) + theme(axis.text.y = element_text(size = 12),
+                                                       axis.text.x = element_text(size = 12, angle = 305,
+                                                                                  vjust = 0.5),
+                                                       axis.title=element_text(size=18,face="bold"))
+
+##Create a figure of CP development among WTSP
+cp.bp %>%
+  mutate(Date = as.Date(Date, format = "%m/%d/%Y"))%>%
+  group_by(Species, Date, CP) %>%
+  mutate(Condition = ifelse(CP == 0, "NB",ifelse(CP == 1, "NB","Breeding")))%>%
+  group_by(Species, Date, Condition)%>%
+  summarize(N = n()) %>%
+  mutate(Percentage = N/sum(N)) %>%
+  complete(Species, Date, Condition, fill = list(N = 0,Percentage = 0)) %>%
+  filter(Species == "WTSP", Condition == "Breeding") %>%
+  ggplot(., aes(x = Date, y = Percentage)) + geom_point() + stat_smooth(method="glm",
+                                                                        method.args = list(family="binomial"), 
+                                                                        formula = y ~ ns(x, 3), level = 0.75) + 
+  ylab("Percentage of captured male White-throated\nSparrows in breeding condition") + 
+  scale_x_date(breaks = date_breaks("4 days")) + theme(axis.text.y = element_text(size = 12),
+                                                       axis.text.x = element_text(size = 12, angle = 305,
+                                                                                  vjust = 0.5),
+                                                       axis.title=element_text(size=18,face="bold"))
+
+##Create a figure of BP development among WTSP
+cp.bp %>%
+  mutate(Date = as.Date(Date, format = "%m/%d/%Y"))%>%
+  group_by(Species, Date, BP) %>%
+  mutate(Condition = ifelse(BP == 0, "NB","Breeding"))%>%
+  group_by(Species, Date, Condition)%>%
+  summarize(N = n()) %>%
+  mutate(Percentage = N/sum(N)) %>%
+  complete(Species, Date, Condition, fill = list(N = 0,Percentage = 0)) %>%
+  filter(Species == "WTSP", Condition == "Breeding") %>%
+  ggplot(., aes(x = Date, y = Percentage)) + geom_point() + stat_smooth(method="glm",
+                                                                        method.args = list(family="binomial"), 
+                                                                        formula = y ~ ns(x, 3), level = 0.75) + 
+  ylab("Percentage of captured female White-throated\nSparrows in breeding condition") + 
+  scale_x_date(breaks = date_breaks("4 days")) + theme(axis.text.y = element_text(size = 12),
+                                                       axis.text.x = element_text(size = 12, angle = 305,
+                                                                                  vjust = 0.5),
+                                                       axis.title=element_text(size=18,face="bold"))
